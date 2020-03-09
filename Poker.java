6b1980b70ded797d42d60d31671600b45ac09566
Emrekannnichtprogrammieren <50622787+Emrekannnichtprogrammieren@users.noreply.github.com>
2020-03-09 08:38:37 +0100

    Add files via upload

--- Poker.java
+++ Poker.java
@@ -16,7 +16,7 @@ public class Poker {
 	public static int[]ziehen(int[]hand) {
 		int[] zieh = new int[deck];
 		for (int i = 0; i < deck; i++) {
-			int randomZahl =hand[((int) (Math.random()*(anzKarten*anzFarben - i)))];
+			int randomZahl = hand[((int) (Math.random()*(anzKarten*anzFarben - i)))];
 			zieh[i]= hand[randomZahl];
 		}
 		return zieh;
@@ -73,7 +73,7 @@ public class Poker {
 	        return AnzahlDoppeltWert(newHand) == 4;
 	    }
 	    
-	    public static boolean Drlling(int[] newHand) {
+	    public static boolean Drilling(int[] newHand) {
 	        return AnzahlDoppeltWert(newHand) == 6;
 	    }
 	    
@@ -85,7 +85,24 @@ public class Poker {
 	        return AnzahlDoppeltTyp(newHand) == 8;
 	    }
 	    
-	    
+	    public static boolean RoyalFlush(int[] newHand) {
+			if (Kartenwert(newHand[0]) == 0 && Kartenwert(newHand[1]) == 9 && Kartenwert(newHand[2]) == 10 &&
+					Kartenwert(newHand[3]) == 11 && Kartenwert(newHand[4]) == 12)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	      
+	    public static int Kartenwert(int Karten) {
+			
+			int Kartenwert=(anzKarten*anzFarben)%13;
+			return Kartenwert;
+		}
+
 	public static void main(String[] args) {
 		int[] hand = new int [anzKarten * anzFarben];
 		befüllen(hand);
@@ -98,6 +115,28 @@ public class Poker {
 			System.out.printf(zug[i] + " ");
 		}
 		
-	}
-
+		if (RoyalFlush(hand)) {
+			System.out.println("Royal Flush");
+		}
+		
+		if (FullHouse(hand)) {
+			System.out.println("Full House");
+		}
+		
+		if (Vierling(hand)) {
+			System.out.println("Vierling");
+		}
+		
+		if (Drilling(hand)) {
+			System.out.println("Drilling");
+		}
+		
+		if (ZweiPaar(hand)) {
+			System.out.println("Zwei Paar");
+		}
+		
+		if (EinPaar(hand)) {
+			System.out.println("Ein Paar");
+		}
+}
 }
